1eddf58b60bcc187caaddc020d1b458bf1a500c5
Lovro Grguric Mileusnic <lovrogm@gmail.com>
2024-06-03 23:51:48 +0200

    Add banner and starting code

--- generate.py
+++ generate.py
@@ -1,2 +1,21 @@
+import yaml
+
+banner = r"""
+   ___  ____  _______ _____   ___ 
+  / _ \/ __ \/ ___/ //_/ _ | / _ \
+ / // / /_/ / /__/ ,< / __ |/ , _/
+/____/\____/\___/_/|_/_/ |_/_/|_| 
+"""
+
+compose = {"services": {}, "networks": {}}
+
 def main():
-    print("Welcome to docKar")
+    print(banner)
+    compose["services"]["serv1"] = None
+    compose["services"]["serv2"] = None
+    compose["networks"]["net1"] = None
+    compose["networks"]["net2"] = None
+    with open("compose.yml", "w") as f:
+        yaml.dump(compose, f, )
+
+main()
